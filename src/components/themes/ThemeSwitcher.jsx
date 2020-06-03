a2b388c5906de32bfe5e2a52dcd08cae1adb7898
OffS3c <jahanzaib.khan.official@gmail.com>
2020-06-03 18:42:01 +0500

    added callback for themeswitcher

--- src/components/themes/ThemeSwitcher.jsx
+++ src/components/themes/ThemeSwitcher.jsx
@@ -8,6 +8,29 @@ import ThemeContext from "./ThemeContext";
 
 export default function ThemeSwitcher(props) {
   const { selectedTheme, setSelectedTheme } = React.useContext(ThemeContext);
+  const componentPropsList = ["cb"];
+  const [componentProps, btnProps] = getProps();
+  const callback = componentProps.cb ? componentProps.cb : () => {};
+  
+  function getProps() {
+    const keys = Object.keys(props);
+    const values = Object.values(props);
+    const myComponentProps = {};
+    const myBtnProps = {};
+
+    keys.forEach((e, i) => {
+      if (componentPropsList.includes(e)) {
+        myComponentProps[e] = values[i];
+      }
+    });
+    keys.forEach((e, i) => {
+      if (!componentPropsList.includes(e)) {
+        myBtnProps[e] = values[i];
+      }
+    });
+
+    return [myComponentProps, myBtnProps];
+  }
 
   function switchToDarkMode() {
     setSelectedTheme(Themes.dark);
@@ -23,10 +46,11 @@ export default function ThemeSwitcher(props) {
     } else {
       switchToLightMode();
     }
+    callback();
   }
 
   return (
-    <IconButton {...props} onClick={toggleTheme}>
+    <IconButton {...btnProps} onClick={toggleTheme}>
       { (selectedTheme.palette.type === 'light') ? <LightModeActiveIcon /> : <DarkModeActiveIcon /> }
     </IconButton>
   );
