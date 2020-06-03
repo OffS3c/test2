7815b6a38b8aedb3954e620138598c95bd9010f9
OffS3c <jahanzaib.khan.official@gmail.com>
2020-06-03 18:43:39 +0500

    removed old blog

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 import Blog from "./blog/Blog";
-import OldBlog from "./blog-old/Main";
 
 export default function Home() {
   return (
     <React.Fragment>
       <Blog />
-      <OldBlog />
     </React.Fragment>
   );
 };
